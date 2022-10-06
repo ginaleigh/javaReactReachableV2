8da5d04693d12b78ae0d66d0ce9edb3723343562
Gina Mirando <gina.mirando5@gmail.com>
2022-10-05 19:34:09 -0500

    changing notes

--- src/components/Stops.js
+++ src/components/Stops.js
@@ -25,23 +25,38 @@ const StopList = () => {
   const [stop, setStop] = useState(null);
   const [line, setLine] = useState(null);
 
+// async function axiosCall() {
+
+//when you map your data in that drop down list I think you need to do line.[property name]
+
+//   await axios.get(`http://localhost:8080/stops`).then((res) => {
+//         const stops = res.data;
+//         setStops(stops);
+//         console.log(stops);
+//       });
+
+
   useEffect(() => {
     axios.get(`http://localhost:8080/lines`).then((res) => {
       const lines = res.data;
       setLines(lines);
-      console.log(lines);
-      // axios.get(`http://localhost:8080/stops`).then((res) => {
-  //       const stops = res.data;
-  //       setStops(stops);
-  //       console.log(stops);
-  //     });
+      // console.log(lines);
+      //map an array of objects???
+      axios.get(`http://localhost:8080/stops`).then((res) => {
+        const stops = res.data;
+        setStops(stops);
+        console.log(stops);
+      });
+    
     });
-  }, []);
+  }, );
+
 
   const handleChange = (event) => {
     setLine(event.target.value);
   };
 
+  //this needs to change to create array and loop through stop_name?
   const handleStopChange = (event) => {
     const stopId = parseInt(event.target.value);
     for (let i = 0; i < stops.length; i++) {
@@ -70,7 +85,7 @@ const StopList = () => {
         // options={[]}
         // value={stop ? stop.id : ""}
       />
-      <p></p> 
+      <p></p>
       <b>Please select a train line and stop</b>
       <b>{stop && <p>This stop is {stop.is_accessible ? "accessible" : "not accessible"}</p>}</b>
       {stop && <ArrivalList stop={stop} />}
@@ -79,3 +94,22 @@ const StopList = () => {
 };
 
 export default StopList;
+
+//How does I make this work and what do I want it to do functionally?
+//Function
+//1. App opens - dropdown contains all lines & all stops
+//2. dropdown line selected & stops filter to that line(color)
+//3. dropdown stop selected & all arrival times with dest are displayed
+//{arrival.staNm} towards {arrival.destNm} with arrT.
+
+//Option 2
+//1. App opens - dropdown with search function presents with From/To instead of Line/Stop
+//2. This dropdown populates all stops in both with line (color) ** station_descriptive_name in /stops
+//3. You pick the from and to stops from the list and FE call the BE to retrieve arrT
+
+//ok, so if 1 makes more sense how do I get stops displayed properly?
+//needs to be returned as array but it bumps down object display
+//    Object { stop_id: 30162, direction_id: "W", stop_name: "18th (54th/Cermak-bound)",
+//Do I need to ???
+//changing the formatting of the api call before returning?
+//or find a way to change formatting above to hold child properly
